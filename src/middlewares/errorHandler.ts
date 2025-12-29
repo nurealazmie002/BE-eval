e921b9bf001f67c7832c1b77883857ce69bb3cbb
nurealazmie002 <nurealazmie002@gmail.com>
2025-12-29 17:50:38 +0700

    fix generate

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { Prisma } from '@prisma/client'; 
+import { Prisma } from '../generated/client'; 
 
 export const errorHandler = (
   err: any,
--- src/services/member.ts
+++ src/services/member.ts
@@ -1,5 +1,5 @@
 import { MemberRepository } from "../repositories/member.repository";
-import { MemberStatus } from "@prisma/client"; 
+import { MemberStatus } from "../generated/client"; 
 
 const memberRepository = new MemberRepository();
 
